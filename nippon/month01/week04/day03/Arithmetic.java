2f77120f825c4111989d9deb62d08d9ee0769bda
Khongoroo <hongoroohongoroo582@gmail.com>
2026-04-22 18:02:40 +0800

    example

---
+++ nippon/month01/week04/day03/Arithmetic.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Arithmetic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1-r too: ");
+        int a = scanner.nextInt();
+        System.out.println("2-r too: " );
+        int b = scanner.nextInt();
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        int g = a % 4;
+
+        System.out.println("a + b: ");
+         
+
+    
+    }
+    
+}
